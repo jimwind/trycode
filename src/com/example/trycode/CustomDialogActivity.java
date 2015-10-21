c4f611f526f58b6b0970e8d6ae55c36cb115a241
jimwind <jimwindgao@gmail.com>
2015-10-21 17:17:17 +0800

    alert dialog

--- src/com/example/trycode/CustomDialogActivity.java
+++ src/com/example/trycode/CustomDialogActivity.java
@@ -3,7 +3,10 @@ package com.example.trycode;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
+import android.widget.TextView;
 
 /**
  *
@@ -13,11 +16,13 @@ import android.view.Window;
  * @Date 2015-10-21
  */
 public class CustomDialogActivity extends Activity {
+	private TextView tvMsg;
+	
     /**
      * Initialization of the Activity after it is first created.  Must at least
      * call {@link android.app.Activity#setContentView setContentView()} to
      * describe what is to be displayed in the screen.
-     */
+     */	
     @Override
 	protected void onCreate(Bundle savedInstanceState) {
         // Be sure to call the super class.
@@ -28,5 +33,16 @@ public class CustomDialogActivity extends Activity {
         // the content of our screen.
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.custom_dialog_activity);
+        tvMsg = (TextView)findViewById(R.id.text);
+        tvMsg.setOnClickListener(onclick);
     }
+    OnClickListener  onclick = new OnClickListener(){
+
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			finish();
+		}
+    	
+    };
 }
--- src/com/example/trycode/MainActivity.java
+++ src/com/example/trycode/MainActivity.java
@@ -1,56 +1,76 @@
 package com.example.trycode;
 
-
 import com.example.util.DateTimePickDialogUtil;
+
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
-	private TextView mTextView;	
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        mTextView = (TextView)findViewById(R.id.select_date_time);
-        mTextView.setOnClickListener(listener);
-    }
-    OnClickListener listener = new OnClickListener(){
+	private Activity mActivity;
+	private TextView select_date_time;
+	private TextView open_alert_dialog;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		mActivity = this;
+		select_date_time = (TextView) findViewById(R.id.select_date_time);
+		select_date_time.setOnClickListener(listener);
+		open_alert_dialog = (TextView) findViewById(R.id.open_alert_dialog);
+		open_alert_dialog.setOnClickListener(listener);
+	}
+
+	OnClickListener listener = new OnClickListener() {
 		@Override
 		public void onClick(View v) {
 			// TODO Auto-generated method stub
-			DateTimePickDialogUtil dateTimePicKDialog = new DateTimePickDialogUtil(  
-				MainActivity.this, mTextView.getText().toString());  
-				dateTimePicKDialog.dateTimePicKDialog(datetimeselect);  
-		}    	
-    };
-    DateTimePickDialogUtil.OnDateTimeSetListener datetimeselect = new DateTimePickDialogUtil.OnDateTimeSetListener(){
+			if (v.getId() == R.id.select_date_time) {
+				DateTimePickDialogUtil dateTimePicKDialog = new DateTimePickDialogUtil(
+						MainActivity.this, select_date_time.getText().toString());
+				dateTimePicKDialog.dateTimePicKDialog(datetimeselect);
+			} else if (v.getId() == R.id.open_alert_dialog) {
+				LinearLayout layout = (LinearLayout) mActivity
+						.getLayoutInflater().inflate(R.layout.custom_dialog_activity, null);
+				new AlertDialog.Builder(mActivity)
+				.setView(layout)
+				.show();
+			}
+
+		}
+	};
+	DateTimePickDialogUtil.OnDateTimeSetListener datetimeselect = new DateTimePickDialogUtil.OnDateTimeSetListener() {
 		@Override
 		public void onDateOrTimeSet(String datetime) {
 			// TODO Auto-generated method stub
-			mTextView.setText(datetime);
-		}    	
-    };
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+			select_date_time.setText(datetime);
+		}
+	};
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 }
